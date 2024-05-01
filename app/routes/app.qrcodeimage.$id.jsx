1bde3b7774c33e59fdf0999d5f03b9632303a3d1
Roc Yu <rociiu.yu@gmail.com>
2024-05-01 14:04:37 +0800

    add api to return qrcode image data directly

---
+++ app/routes/app.qrcodeimage.$id.jsx
@@ -0,0 +1,26 @@
+import qrcode from "qrcode";
+import { json } from "@remix-run/node";
+import invariant from "tiny-invariant";
+
+import db from "../../db.server";
+
+export async function loader({ params }) {
+  var id = params.id;
+  const qrCode = await db.qRCode.findUnique({
+    where: {
+      id: id
+    }
+  });
+
+  invariant(qrCode, "Could not find QR code destination");
+
+  const url = new URL(`/qrcodes/${id}/scan`, process.env.SHOPIFY_APP_URL);
+  const qrData = await qrcode.toDataURL(url.href);
+
+  return json(null, {
+    headers: {
+      "Content-Type": "image/png", // Specify the image type
+    },
+    body: qrData,
+  });
+}
