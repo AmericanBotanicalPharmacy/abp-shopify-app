16b413c4b28fc94750de05a965fcd44207659f10
Roc Yu <rociiu.yu@gmail.com>
2024-05-01 15:01:04 +0800

    update api on generating image file

--- app/routes/qrcodeimage.$id.jsx
+++ app/routes/qrcodeimage.$id.jsx
@@ -1,6 +1,7 @@
 import qrcode from "qrcode";
 import { json } from "@remix-run/node";
 import invariant from "tiny-invariant";
+import { fs } from 'fs';
 
 import db from "../db.server";
 
@@ -15,12 +16,15 @@ export async function loader({ params }) {
   invariant(qrCode, "Could not find QR code destination");
 
   const url = new URL(`/qrcodes/${id}/scan`, process.env.SHOPIFY_APP_URL);
-  const qrData = await qrcode.toDataURL(url.href);
+  await qrcode.toFile(url.href);
 
-  return {
+  const filename = "qrcode_" + id + ".png"
+  await qrcode.toFile(filename, data, { width: 256, type: 'png' });
+
+  const imageBuffer = await fs.promises.readFile(filename);
+  return new Response(imageBuffer, {
     headers: {
-      "Content-Type": "image/png", // Specify the image type
+      "Content-Type": "image/png",
     },
-    body: qrData,
-  };
+  })
 }
