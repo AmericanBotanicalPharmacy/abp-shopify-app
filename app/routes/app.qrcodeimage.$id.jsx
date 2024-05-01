1df4df39125814cc4f717f1c2f0aecfe28c8c990
Roc Yu <rociiu.yu@gmail.com>
2024-05-01 14:12:52 +0800

    fix import path

--- app/routes/app.qrcodeimage.$id.jsx
+++ app/routes/app.qrcodeimage.$id.jsx
@@ -2,7 +2,7 @@ import qrcode from "qrcode";
 import { json } from "@remix-run/node";
 import invariant from "tiny-invariant";
 
-import db from "../../db.server";
+import db from "../db.server";
 
 export async function loader({ params }) {
   var id = params.id;
