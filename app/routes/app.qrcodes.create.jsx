362945551324ec4de076b592efd7e712a39f5f2a
Roc Yu <rociiu.yu@gmail.com>
2024-05-07 15:59:16 +0800

    implement api for create qrcode

--- app/routes/app.qrcodes.create.jsx
+++ app/routes/app.qrcodes.create.jsx
@@ -9,6 +9,8 @@ import {
   useParams,
 } from "@remix-run/react";
 import { authenticate } from "../shopify.server";
+import db from "../db.server";
+
 import {
   Card,
   Bleed,
@@ -35,18 +37,24 @@ export async function action({ request, params }) {
   const { session, admin } = await authenticate.admin(request);
   const { shop } = session;
 
-  console.log(request)
+  const requestJson = await request.json()
 
-  const response = await admin.rest.get({ path: "products/"+productId+".json" });
+  const response = await admin.rest.get({ path: "products/"+requestJson.product_id+".json" });
   const responseJson = await response.json();
+
   console.log(responseJson)
+
   const product =responseJson.product
   if(product) {
     const { images, id, variants, title, handle } = product;
 
-
     const data = {
-
+      productId: id,
+      productVariantId: variants[0].id,
+      productTitle: title,
+      productHandle: handle,
+      productAlt: product.image?.alt,
+      productImage: product.image?.src
     }
     await db.qRCode.create({ data })
   }
