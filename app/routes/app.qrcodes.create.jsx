b9141cd39b2617ec49998a79fc3e4d21e5a298bc
Roc Yu <rociiu.yu@gmail.com>
2024-05-07 14:39:30 +0800

    Update qrcodes api

--- app/routes/app.qrcodes.create.jsx
+++ app/routes/app.qrcodes.create.jsx
@@ -32,10 +32,24 @@ import db from "../db.server";
 import { getQRCode, validateQRCode } from "../models/QRCode.server";
 
 export async function action({ request, params }) {
-  const { session } = await authenticate.admin(request);
+  const { session, admin } = await authenticate.admin(request);
   const { shop } = session;
 
   console.log(request)
+
+  const response = await admin.rest.get({ path: "products/"+productId+".json" });
+  const responseJson = await response.json();
+  console.log(responseJson)
+  const product =responseJson.product
+  if(product) {
+    const { images, id, variants, title, handle } = product;
+
+
+    const data = {
+      
+    }
+    await db.qRCode.create({ data })
+  }
 
   return cors(json({
     success: true
