0a7137f648e0beadccf446bf0bea738a509c8e9c
Roc Yu <rociiu.yu@gmail.com>
2024-07-04 11:01:54 +0800

    Revert "add app in the provider to fix app error"
    This reverts commit 3e0b7f5cc5007abf3b81b8840d3695d8b66cea08.

--- app/routes/app.jsx
+++ app/routes/app.jsx
@@ -4,9 +4,7 @@ import { boundary } from "@shopify/shopify-app-remix/server";
 import { AppProvider } from "@shopify/shopify-app-remix/react";
 import polarisStyles from "@shopify/polaris/build/esm/styles.css?url";
 import {I18nContext, I18nManager} from '@shopify/react-i18n';  // <-- add code
-import { AppProvider as PolarisProvider } from '@shopify/polaris';
 import { AppProvider as DiscountProvider } from "@shopify/discount-app-components";
-import { Provider as AppBridgeProvider } from '@shopify/app-bridge-react';
 
 import { authenticate } from "../shopify.server";
 
@@ -18,13 +16,6 @@ export const loader = async ({ request }) => {
   return json({ apiKey: process.env.SHOPIFY_API_KEY || "" });
 };
 
-const app = createApp({
-  apiKey: process.env.SHOPIFY_API_KEY || "",
-  shopOrigin: new URLSearchParams(window.location.search).get('shop'),
-  forceRedirect: true,
-});
-
-
 export default function App() {
   const { apiKey } = useLoaderData();
   const i18nManager = new I18nManager({   // <-- add code
@@ -33,19 +24,17 @@ export default function App() {
 
   return (
       <AppProvider isEmbeddedApp apiKey={apiKey}>
-        <AppBridgeProvider config={app}>
-          <DiscountProvider locale="en-US" ianaTimezone="America/Los_Angeles">
-            <I18nContext.Provider value={i18nManager}> 
-              <ui-nav-menu>
-                <Link to="/app" rel="home">
-                  Home
-                </Link>
-                <Link to="/app/additional">Additional page</Link>
-              </ui-nav-menu>
-              <Outlet />
-            </I18nContext.Provider>
-          </DiscountProvider>
-        </AppBridgeProvider>
+        <DiscountProvider locale="en-US" ianaTimezone="America/Los_Angeles">
+          <I18nContext.Provider value={i18nManager}> 
+            <ui-nav-menu>
+              <Link to="/app" rel="home">
+                Home
+              </Link>
+              <Link to="/app/additional">Additional page</Link>
+            </ui-nav-menu>
+            <Outlet />
+          </I18nContext.Provider>
+        </DiscountProvider>
       </AppProvider>
   );
 }
