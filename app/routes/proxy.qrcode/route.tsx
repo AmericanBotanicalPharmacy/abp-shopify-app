932badc25ce755e1e5ba2187738ef9174e41d36d
Roc Yu <rociiu.yu@gmail.com>
2024-07-15 18:36:28 +0800

    migrate proxy.qrcode/route to typescript

--- app/routes/proxy.qrcode/route.tsx
+++ app/routes/proxy.qrcode/route.tsx
@@ -1,14 +1,15 @@
 import { json } from "@remix-run/node";
 import invariant from "tiny-invariant";
+import type { ActionFunctionArgs, LoaderFunctionArgs } from "@remix-run/node";
 
 import db from "../../db.server";
 import { getQRCodeImage } from "../../models/QRCode.server";
 
-export async function loader({ request }) {
+export const loader = async ({ request }: LoaderFunctionArgs) => {
   const url = new URL(request.url);
   var productId = url.searchParams.get("product_id");
   const qrCode = await db.qRCode.findFirst({ where: { productId: {
-    contains: productId
+    contains: productId as string
   } } });
 
   invariant(qrCode, "Could not find QR code destination");
